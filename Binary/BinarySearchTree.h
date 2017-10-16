f6b1e2738b93fada4c69bdf78e37cd96b0c8f36f
xuhewen <xuhewensuse@outlook.com>
2017-10-16 15:37:21 +0800

    heap

--- Binary/BinarySearchTree.h
+++ Binary/BinarySearchTree.h
@@ -23,8 +23,8 @@ private:
 
     Node* insert(Node *root, Key key, Value value) {
         if(root == NULL) {
-            return new Node(key, value);
             count++;
+            return new Node(key, value);
         }
 
         if(root->key == key)
@@ -33,6 +33,30 @@ private:
             root->left = insert(root, key, value);
         else
             root->right = insert(root, key, value);
+
+        return root;
+    }
+
+    bool contian(Node* root, Key key) {
+        if(root == NULL)
+            return false;
+        if(root->key == key)
+            return true;
+        else if(root->key > key)
+            return contain(root->left, key);
+        else
+            return contain(root->right, key);
+    }
+
+    Value* search(Node *root, Key key) {
+        if(root == NULL)
+            return NULL;
+        if(root->key == key)
+            return &(root->value);
+        else if(key < root->key)
+            return search(root->left, key);
+        else
+            return search(root->right, key);
     }
 
 public:
@@ -51,6 +75,34 @@ public:
 
     void insert(Key key, Value value) {
         insert(root, key, value);
+    }
+
+    void insert2(Key key, Value value) {
+        if(root == NULL) {
+            count++;
+            root = new Node(key, value);
+            return;
+        }
+        Node* p = root;
+        while(p != NULL) {
+            if(p->key == key)
+                p->value = value;
+            else if(p->key > key)
+                p = p->left;
+            else
+                p = p->right;
+        }
+        p = new Node(key, value);
+        count++;
+        return;
+    }
+
+    bool contain(Key key) {
+        return contain(root, key);
+    }
+
+    Value* search(Key key) {
+        return search(root, key)
     }
 
     ~BinarySearchTree() {
